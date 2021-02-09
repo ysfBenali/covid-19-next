9c9f10f1e4471ab279270943a971e3c64101ce9c
Youssef Benali <youyssef@gmail.com>
2021-02-10 00:03:54 +0100

    integrate NProgress when changing routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import Footer from "../common/components/Footer";
 import Head from "next/head";
+import Router from 'next/router';
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import "../styles/globals.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
        <Head>
-        <title>covid-19-next</title>
+        <title>covid-19</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
